c065c8bf994732c89a1f971ad04673c5bed50a1d
VAIBHAV JAIN <64785307+vibhu0209@users.noreply.github.com>
2021-10-08 11:07:00 +0530

    Update DailyPic.js

--- screens/DailyPic.js
+++ screens/DailyPic.js
@@ -1,10 +1,7 @@
 import React, { Component } from 'react';
 import { Text, View, Image, ImageBackground, TouchableOpacity, StyleSheet, Alert, Platform, StatusBar, SafeAreaView, Linking, ScrollView } from 'react-native';
 
-// import axios from 'axios';
-// import {axios} from 'axios';
-// import axios from axios;
-// import "axios" from axios;
+import axios from 'axios';
 
 export default class DailyPicScreen extends Component {
     constructor(props) {
@@ -29,28 +26,8 @@ export default class DailyPicScreen extends Component {
             })
     }
 
-    renderImage = (url) => {
-
-        // <Image source={{ "uri": url }} 
-        // style={{ width: "100%", height: 300, 
-        //borderRadius: 20, margin: 3 }}>
-        // </Image>
-
-        // <Image source={ "uri": url }
-        // style={{ width: "100%", height: 300, 
-        //borderRadius: 20, margin: 3 }}>
-        // </Image>
-        
-        // <Image source={{ "uri"= url }} 
-        // style={{ width: "100%", height: 300, 
-        //borderRadius: 20, margin: 3 }}>
-        // </Image>
-        
-        // <Image require={{ "uri": url }} 
-        // style={{ width: "100%", height: 300, 
-        //borderRadius: 20, margin: 3 }}>
-        // </Image>
-        
+  renderImage = (url) => {
+        <Image source={{ "uri": url }} style={{ width: "100%", height: 300, borderRadius: 20, margin: 3 }}></Image>
     }
 
     renderVideo = () => {
